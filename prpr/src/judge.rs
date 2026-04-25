15f2ae5f0bba6006a7e08f42f47161d315ebbcdc
Weilai114 <wushuo_2009@163.com>
2026-04-25 08:30:43 +0800

    Update judge.rs

--- prpr/src/judge.rs
+++ prpr/src/judge.rs
@@ -17,13 +17,13 @@ use std::{cell::RefCell, collections::HashMap, mem, num::FpCategory};
 use tracing::debug;
 
 pub const FLICK_SPEED_THRESHOLD: f32 = 0.8;
-pub const LIMIT_PERFECT: f64 = 0.04;
-pub const LIMIT_GOOD: f64 = 0.075;
-pub const LIMIT_BAD: f64 = 0.14;
+pub const LIMIT_PERFECT: f64 = 0.055;
+pub const LIMIT_GOOD: f64 = 0.090;
+pub const LIMIT_BAD: f64 = 0.155;
 pub const UP_TOLERANCE: f64 = 0.05;
 pub const DIST_FACTOR: f64 = 0.2;
 
-const EARLY_OFFSET: f64 = 0.0;
+const EARLY_OFFSET: f64 = -0.05;
 
 #[derive(Debug, Clone)]
 pub enum HitSound {
